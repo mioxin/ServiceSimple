85f5fb537e9621703145d7c9e2cadd99f1fd0cf3
MrMioxin <Mioxin@ya.ru>
2015-02-16 15:59:30 +0600

    using executors with 3 threads

--- app/src/main/java/com/example/palchuk/servicesimple/MainActivity.java
+++ app/src/main/java/com/example/palchuk/servicesimple/MainActivity.java
@@ -40,7 +40,10 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void onclickStart(View view) {
-        startService(new Intent(this, MyService.class));
+        //startService(new Intent(this, MyService.class));
+        startService(new Intent(this, MyService.class).putExtra("time", 7));
+        startService(new Intent(this, MyService.class).putExtra("time", 2));
+        startService(new Intent(this, MyService.class).putExtra("time", 4));
     }
 
     public void onclickStop(View view) {
